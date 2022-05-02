a65effea436cc73131b3b973f96c435dd87c8f27
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-02 12:16:41 +0600

    initial header design done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,49 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h2 className="text-primary">This is header</h2>
-    </div>
+    <header className="sticky-top">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="my-header"
+        variant="light"
+      >
+        <Container>
+          <Navbar.Brand as={Link} className="text-danger fw-bold" to="/">
+            BIKE HOUSE
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blogs">
+                Blogs
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              {/* toggle login and sign out  */}
+              {/* {user ? (
+                <Nav.Link onClick={() => signOut(auth)} as={Link} to="/login">
+                  Sign Out
+                </Nav.Link>
+              ) : (
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+              )} */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 

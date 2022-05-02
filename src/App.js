6a5a8455bb96373b2ffbd214f437e27f4ea65f7f
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-02 12:03:56 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blogs from "./Pages/Blogs/Blogs";
+import Home from "./Pages/Home/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
+import NotFound from "./Pages/Shared/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello world</h2>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }

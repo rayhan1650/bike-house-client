560d7a2f0311e3cbe5bcb3731e19a5cf8962d2e5
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-03 19:52:00 +0600

    login functionality implemented

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,16 +1,39 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Login.css";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
 
 const Login = () => {
   const emailRef = useRef("");
   const passwordRef = useRef("");
+  const [sendPasswordResetEmail, sending, passwordResetError] =
+    useSendPasswordResetEmail(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+
+  let location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+
+  if (loading || sending) {
+    return <Loading />;
+  }
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
   };
   return (
     <div className="my-login mx-auto">
@@ -40,19 +63,30 @@ const Login = () => {
           Login
         </Button>
       </Form>
+
+      {/* signin error  */}
+      {error ? <p className="text-danger">Error: {error.message}</p> : <p></p>}
       <p className="mt-2 my-link">
         New to Tech Tutor? <Link to="/signup">Please SignUp.</Link>{" "}
       </p>
+
+      {/* reset password error show */}
+      {passwordResetError ? (
+        <p className="text-danger">Error: {passwordResetError.message}</p>
+      ) : (
+        <p></p>
+      )}
+
       <p className="mt-2 my-link">
         Forget Password?
         <button
-          //   onClick={async () => {
-          //     const email = emailRef.current.value;
-          //     await sendPasswordResetEmail(email);
-          //     if (email) {
-          //       toast("Sent email");
-          //     }
-          //   }}
+          onClick={async () => {
+            const email = emailRef.current.value;
+            await sendPasswordResetEmail(email);
+            if (email) {
+              // toast("Sent email");
+            }
+          }}
           className="border-0 text-primary bg-transparent"
         >
           Reset password

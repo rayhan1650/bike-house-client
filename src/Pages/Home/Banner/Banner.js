f2fa6212e387eee0669be465bf3925dd09c56c44
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-06 11:51:27 +0600

    banner part done

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="my-banner d-flex align-items-center img-fluid ">
+      <h2 className="text-primary fw-bold">Hello world</h2>
+    </div>
+  );
+};
+
+export default Banner;

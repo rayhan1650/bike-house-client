dbff53a7ffda98d7460ae2b13b1502bfe5b8ca08
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-07 22:56:55 +0600

    stock update page design done

--- src/Pages/StockUpdate/StockUpdate.js
+++ src/Pages/StockUpdate/StockUpdate.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import "./StockUpdate.css";
 
 const StockUpdate = () => {
   const { id } = useParams();
@@ -10,10 +12,56 @@ const StockUpdate = () => {
       .then((res) => res.json())
       .then((data) => setItem(data));
   }, []);
+
+  const handleRestock = (event) => {
+    event.preventDefault();
+    const num = event.target.number.value;
+    event.reset();
+    console.log(num);
+  };
   return (
-    <div>
-      <h2>StockUpdate {item.name}</h2>
-      <img src={item.img} alt="" />
+    <div className="container my-stockupdate">
+      <div className="card my-5 shadow">
+        <div className="row g-0 ">
+          <div className="col-md-4 p-2 d-flex justify-content-center align-items-center">
+            <img src={item.img} className="img-fluid " alt="..." />
+          </div>
+          <div className="col-md-8 bg-primary bg-opacity-25">
+            <div className="card-body ">
+              <h4 className="card-title">{item.name}</h4>
+              <span className="badge bg-success">Price: ${item.price}</span>
+              <h5 className="mt-3">Supplier: {item.supplier}</h5>
+              <h5>Quantity: {item.quantity}</h5>
+              <p className="card-text my-text-justify">
+                <strong>Description:</strong>
+                {item.description}
+              </p>
+
+              <div className="card-text d-flex flex-column flex-md-row justify-content-between">
+                <Form onSubmit={handleRestock}>
+                  <InputGroup className="w-auto">
+                    <FormControl
+                      placeholder="Enter Number"
+                      type="number"
+                      name="number"
+                      aria-label="Recipient's username"
+                      aria-describedby="basic-addon2"
+                    />
+                    <Button
+                      type="submit"
+                      variant="outline-primary"
+                      id="button-addon2"
+                    >
+                      Restock
+                    </Button>
+                  </InputGroup>
+                </Form>
+                <button className="btn btn-primary my-margin">Delivered</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

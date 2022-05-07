7cdace9e7b7efaaf6ae1e0c3fecb67529986b7fa
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-07 19:53:25 +0600

    load single data from server

--- src/Pages/StockUpdate/StockUpdate.js
+++ src/Pages/StockUpdate/StockUpdate.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const StockUpdate = () => {
   const { id } = useParams();
+  const [item, setItem] = useState({});
+  useEffect(() => {
+    const url = `http://localhost:5000/inventory/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+  }, []);
   return (
     <div>
-      <h2>StockUpdate {id}</h2>
+      <h2>StockUpdate {item.name}</h2>
+      <img src={item.img} alt="" />
     </div>
   );
 };

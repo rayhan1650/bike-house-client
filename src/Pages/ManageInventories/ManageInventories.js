98a9955365e3b2a228634ea0efd0029980c1cd02
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-08 07:31:57 +0600

    manage inventories page design done

--- src/Pages/ManageInventories/ManageInventories.js
+++ src/Pages/ManageInventories/ManageInventories.js
@@ -1,9 +1,48 @@
 import React from "react";
+import useInventories from "../../hooks/useInventories";
+import { MdDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const ManageInventories = () => {
+  const [inventories, setInventories] = useInventories();
   return (
-    <div>
-      <h3>Manage Inventories</h3>
+    <div className="container">
+      <h2 className="text-center text-primary mt-5">Manage Inventories</h2>
+      <div className="text-center my-3">
+        <Link className="btn btn-outline-success" to="/additem">
+          Add New Item
+        </Link>
+      </div>
+
+      <div className="table-responsive-sm">
+        <table className="table table-bordered border-dark table-striped table-success">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Price</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inventories.map((inventory) => (
+              <tr key={inventory._id}>
+                <td>{inventory.name}</td>
+                <td>${inventory.price}</td>
+                <td>{inventory.quantity}</td>
+                <td>
+                  <button
+                    title="Click here to delete this item."
+                    className="border-0 rounded-circle btn btn-outline-light"
+                  >
+                    <MdDelete className="text-danger fs-5" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

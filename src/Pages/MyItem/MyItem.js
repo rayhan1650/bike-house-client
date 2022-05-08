755051e138a4d4b89a0648dcbbf72fd6ee2d2358
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-05-08 14:17:41 +0600

    myitems page design implemented

---
+++ src/Pages/MyItem/MyItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const MyItem = ({ myItem }) => {
+  const { img, price, name, description, _id, quantity, supplier } = myItem;
+  return (
+    <div className="col ">
+      <div className="card rounded-3 shadow h-100">
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body d-flex flex-column">
+          <h4 className="card-title">{name}</h4>
+          <span className="badge bg-success fs-4 text-start">
+            Price: ${price}
+          </span>
+          <h5 className="mt-3">Quantity: {quantity}</h5>
+          <h5>Supplier: {supplier}</h5>
+          <p className="card-text my-text-justify my-2">
+            {description.length > 250
+              ? `${description.slice(0, 249)} ....`
+              : description}
+          </p>
+
+          <button className="btn btn-primary mt-auto">Delete Item</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyItem;
--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import "./MyItems.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import MyItem from "../MyItem/MyItem";
 
 const MyItems = () => {
   const [user] = useAuthState(auth);
   const email = user.email;
-  const [myItems, setMyItems] = useState({});
+  const [myItems, setMyItems] = useState([]);
   useEffect(() => {
     const url = `http://localhost:5000/inventories?email=${email}`;
     fetch(url)
@@ -14,8 +15,13 @@ const MyItems = () => {
       .then((data) => setMyItems(data));
   }, []);
   return (
-    <div className="myItems">
-      <h2>MyItems {myItems[0]?.name}</h2>
+    <div className="container myItems my-5">
+      <h2 className="text-center mb-4 text-primary fw-bold">My Items</h2>
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        {myItems.length
+          ? myItems.map((myItem) => <MyItem key={myItem._id} myItem={myItem} />)
+          : "You have no items."}
+      </div>
     </div>
   );
 };
